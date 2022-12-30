d0f13c638ce751b751718f0bbf81d0f1b0053b5f
Chandrakana Nandi <chandra.nandi90@gmail.com>
2022-12-29 20:55:55 -0800

    readme and bunch of nits before release

---
+++ benchmarks/MultipleContracts/C.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.12;
+
+library Utils {
+    function getarray(address[] memory c, address e) internal view {
+        assert(c[0] == e);    
+   
+    }
+
+}
+contract C {
+   
+    function foo() external view returns (address[] memory)  {
+        address[] memory a = new address[](1);
+        a[0] = msg.sender;
+        return a;
+    }
+
+    function get10PowerDecimals(uint8 decimals) public pure returns (uint256) {
+        uint256 a = 10;
+        uint256 res = a ** decimals;
+        return res;
+    }
+
+     function getarray(address[] memory c, address e) public view {
+        assert(c[0] == e);    
+   
+    }
+   
+    function callmyself() external {
+        address[] memory b = this.foo();
+        Utils.getarray(b,address(this));
+    }
+}
