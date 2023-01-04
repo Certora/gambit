d4ce85c420a459fb068088ada61be0b1a1a187ef
Chandrakana Nandi <chandra.nandi90@gmail.com>
2023-01-03 21:56:19 -0800

    use io::result

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use clap::{Parser, ValueEnum};
+use clap::{Error, Parser, ValueEnum};
 use rand::SeedableRng;
 use rand_pcg::Pcg64;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashSet;
 use std::fmt::Debug;
-use std::fs;
 use std::io::BufReader;
+use std::{fs, io};
 use std::{
     fs::File,
     path::{Path, PathBuf},
@@ -42,16 +42,15 @@ impl MutantGenerator {
     }
 
     /// Compile the input solc files and get json ASTs.
-    pub fn compile_solc(&self, sol: &String, out: PathBuf) -> SolAST {
-        let norms_to_path = get_path_normals(sol);
-        assert!(norms_to_path.is_some());
-        let norm_path = norms_to_path.unwrap();
-        let norm_sol = norm_path.to_str().unwrap_or_else(|| {
-            panic!("Could not convert the path to the sol file to a normalized version.")
-        });
+    pub fn compile_solc(&self, sol: &String, out: PathBuf) -> Result<SolAST, Error> {
+        let norms_of_path =
+            get_path_normals(sol).unwrap_or_else(|| panic!("Path to sol file is broken"));
+        if norms_of_path.extension().unwrap().eq("sol") {
+            panic!("{} is not a solidity source file.", sol);
+        }
+        let norm_sol = norms_of_path.to_str().unwrap();
         let sol_path = out.join("input_json/".to_owned() + norm_sol);
-        std::fs::create_dir_all(sol_path.parent().unwrap())
-            .expect("Unable to create directory for storing input jsons.");
+        std::fs::create_dir_all(sol_path.parent().unwrap())?;
         log::info!(
             "made parent directories for writing the json ast at {}.",
             sol_path.to_str().unwrap()
@@ -74,7 +73,7 @@ impl MutantGenerator {
             .unwrap_or_else(|| panic!("solc terminated with a signal."))
             != 0
         {
-            panic!("Failed to compile source. Try with a different version of solc.")
+            panic!("Failed to compile source. Maybe try with a different version of solc (e.g., --solc solc8.10)")
         }
         let ast_fnm = Path::new(sol)
             .file_name()
@@ -85,37 +84,35 @@ impl MutantGenerator {
             + "_json.ast";
         let ast_path = sol_path.join(&ast_fnm);
         let json_fnm = sol_path.join(ast_fnm + ".json");
-        std::fs::copy(ast_path, &json_fnm).expect("Could not copy .ast content to .json");
+        std::fs::copy(ast_path, &json_fnm)?;
         let json_f = File::open(&json_fnm).unwrap_or_else(|_| {
             panic!("Cannot open the json file {}", &json_fnm.to_str().unwrap())
         });
         let ast_json: Value =
             serde_json::from_reader(json_f).expect("AST json is not well-formed.");
-        SolAST {
+        Ok(SolAST {
             element: Some(ast_json),
             contract: None,
-        }
+        })
     }
 
     /// Create a directory for saving the mutants.
-    fn mk_mutant_dir(&self, fnm: &str) {
+    fn mk_mutant_dir(&self, fnm: &str) -> io::Result<()> {
         let norm_path = get_path_normals(fnm);
         assert!(norm_path.is_some());
         let mut_dir = PathBuf::from(&self.params.outdir).join(norm_path.unwrap());
         if let Some(pd) = mut_dir.parent() {
             if pd.is_dir() {
-                fs::remove_dir_all(pd)
-                    .expect("Directory existed but was unable to remove content.");
+                fs::remove_dir_all(pd)?;
             }
         }
-        std::fs::create_dir_all(mut_dir.parent().unwrap())
-            .expect("Unable to create output directory.");
+        std::fs::create_dir_all(mut_dir.parent().unwrap())?;
+        Ok(())
     }
 
     /// Create directories for mutants from a json config file.
-    fn mutant_dirs_from_json(&self) {
-        let f =
-            File::open(&self.params.json.as_ref().unwrap()).expect("Cannot open json config file.");
+    fn mutant_dirs_from_json(&self) -> io::Result<()> {
+        let f = File::open(&self.params.json.as_ref().unwrap())?;
         let config: Value = serde_json::from_reader(BufReader::new(f)).expect("Ill-formed json.");
         match config {
             Value::Array(elems) => {
@@ -123,13 +120,16 @@ impl MutantGenerator {
                 for e in elems {
                     paths.insert(e["filename"].as_str().unwrap().to_string());
                 }
-                paths.iter().for_each(|p| self.mk_mutant_dir(p));
+                paths.iter().for_each(|p| {
+                    self.mk_mutant_dir(p).ok();
+                });
             }
             Value::Object(o) => {
-                self.mk_mutant_dir(o["filename"].as_str().unwrap());
+                self.mk_mutant_dir(o["filename"].as_str().unwrap())?;
             }
             _ => panic!("Ill-formed json."),
         }
+        Ok(())
     }
 
     /// Generate mutations for a single file.
@@ -139,13 +139,19 @@ impl MutantGenerator {
         muts: Option<Vec<String>>,
         funcs: Option<Vec<String>>,
         contract: Option<String>,
-    ) {
+    ) -> io::Result<()> {
         let rand = self.rng.clone();
         let outdir = Path::new(&self.params.outdir);
-        let ast = self.compile_solc(file_to_mutate, outdir.to_path_buf());
+        let ast = self
+            .compile_solc(file_to_mutate, outdir.to_path_buf())
+            .ok()
+            .unwrap();
         let mut_types = muts.map_or(MutationType::value_variants().to_vec(), |ms| {
             ms.iter()
-                .map(|m| MutationType::from_str(m, true).unwrap())
+                .map(|m| {
+                    MutationType::from_str(m, true)
+                        .unwrap_or_else(|_| panic!("Could not generate mutant type from {}.", m))
+                })
                 .collect()
         });
 
@@ -192,16 +198,16 @@ impl MutantGenerator {
                 None => false,
             }
         };
-
         run_mutation.get_mutations(is_valid);
+        Ok(())
     }
 
     /// Run Gambit from a json config file.
-    fn run_from_config(&mut self, cfg: &String) {
-        self.mutant_dirs_from_json();
-        let f = File::open(cfg).expect("Cannot open json config file.");
-        let config: Value = serde_json::from_reader(BufReader::new(f)).expect("Ill-formed json.");
-        let mut process_single_file = |v: &Value| {
+    fn run_from_config(&mut self, cfg: &String) -> io::Result<()> {
+        self.mutant_dirs_from_json()?;
+        let f = File::open(cfg)?;
+        let config: Value = serde_json::from_reader(BufReader::new(f))?;
+        let mut process_single_file = |v: &Value| -> io::Result<()> {
             if let Some(filename) = &v.get("filename") {
                 let mut funcs_to_mutate: Option<Vec<String>> = None;
                 let mut selected_muts: Option<Vec<String>> = None;
@@ -243,35 +249,40 @@ impl MutantGenerator {
                         funcs_to_mutate = fs.into();
                     }
                 }
-                self.run_one(&fnm.to_string(), selected_muts, funcs_to_mutate, contract);
+                self.run_one(&fnm.to_string(), selected_muts, funcs_to_mutate, contract)?;
             }
+            Ok(())
         };
         match config {
             Value::Array(elems) => {
                 for elem in elems {
-                    process_single_file(&elem);
+                    process_single_file(&elem)?;
                 }
             }
-            Value::Object(_) => process_single_file(&config),
+            Value::Object(_) => {
+                process_single_file(&config)?;
+            }
             _ => panic!("Ill-formed json."),
         }
+        Ok(())
     }
 
     /// Main runner
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> io::Result<()> {
         log::info!("starting run()");
         let files = &self.params.filename;
         let json = &self.params.json.clone();
         if files.is_some() {
             for f in files.as_ref().unwrap() {
-                self.mk_mutant_dir(&f.to_string());
-                self.run_one(f, None, None, None);
+                self.mk_mutant_dir(&f.to_string())?;
+                self.run_one(f, None, None, None)?;
             }
         } else if json.is_some() {
-            self.run_from_config(json.as_ref().unwrap());
+            self.run_from_config(json.as_ref().unwrap())?;
         } else {
             panic!("Must provide either --filename file.sol or --json config.json.")
         }
+        Ok(())
     }
 }
 
@@ -318,12 +329,13 @@ pub enum Command {
 }
 
 /// Entry point
-fn main() {
+fn main() -> io::Result<()> {
     let _ = env_logger::builder().try_init();
     match Command::parse() {
         Command::Mutate(params) => {
             let mut mutant_gen = MutantGenerator::new(params);
-            mutant_gen.run();
+            mutant_gen.run()?;
         }
     }
+    Ok(())
 }
