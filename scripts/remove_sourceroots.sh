6a0d8bb42f34c61cfbe50923b52b5f37e9bdfe18
Ben Kushigian <ben.kushigian@certora.com>
2023-05-10 13:11:41 -0700

    check OS before running sed

--- scripts/remove_sourceroots.sh
+++ scripts/remove_sourceroots.sh
@@ -5,4 +5,11 @@
 #
 # remove sourceroot fields from JSON since these are absolute paths
 
-sed -i "" '/"sourceroot":/d' "$1"
+if [[ "$(uname)" == "Linux" ]]; then
+    sed -i '/"sourceroot":/d' "$1"
+elif [[ "$(uname)" == "Darwin" ]]; then
+    sed -i "" '/"sourceroot":/d' "$1"
+else
+    echo "Unknown operating system: using the GNU sed interface"
+    sed -i '/"sourceroot":/d' "$1"
+fi
