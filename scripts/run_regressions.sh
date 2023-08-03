721dd4495226127bbb52b8a09a542c6ac52be840
Ben Kushigian <ben.kushigian@certora.com>
2023-08-03 14:56:28 -0700

    Suppress shellcheck warnings

--- scripts/run_regressions.sh
+++ scripts/run_regressions.sh
@@ -19,6 +19,8 @@
 # `resources/regressions/XXXXX`) relative to this script's parent directory.
 
 SCRIPTS=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+green_check=""
+red_x=""
 # shellcheck disable=SC1091
 source "$SCRIPTS/util.sh"
 GAMBIT="$SCRIPTS/.."
--- scripts/util.sh
+++ scripts/util.sh
@@ -5,3 +5,7 @@
 green_check="$(printf "[\033[32;1m ✔ \033[0m]")"
 yellow_elipses="$(printf "[\033[33;1m...\033[0m]")"
 red_x="$(printf "[\033[31;1m ✘ \033[0m]")"
+
+export green_check
+export yellow_elipses
+export red_x
