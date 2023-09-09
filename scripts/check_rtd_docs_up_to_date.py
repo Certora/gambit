5bbb07fc3b999cb7e13fdf3cbcdc6b893d3f5b23
Ben Kushigian <ben.kushigian@certora.com>
2023-09-09 15:33:41 -0700

    Fixed scripts (pulled changes from fix_readme)

--- scripts/check_rtd_docs_up_to_date.py
+++ scripts/check_rtd_docs_up_to_date.py
@@ -20,14 +20,38 @@ def main():
     parser.add_argument(
         "--branch", default="master", help="Branch to check README from"
     )
+    parser.add_argument(
+        "--no_colors", action="store_true", help="Do not use ansi color on outputs"
+    )
     args = parser.parse_args()
 
-    exit_code = check_rtd_docs_up_to_date(branch=args.branch)
+    exit_code = check_rtd_docs_up_to_date(branch=args.branch, colors=not args.no_colors)
 
     sys.exit(exit_code)
 
 
-def check_rtd_docs_up_to_date(branch="master") -> int:
+def print_unified_diff(diff, colors=True):
+    color_fn = {}
+    if colors:
+        try:
+            from ansi.color import fg
+
+            color_fn = {"+": fg.green, "-": fg.red, "@": fg.blue}
+        except ImportError:
+            colors = False
+
+    for line in diff:
+        l: str = line
+        if colors:
+            if l.startswith("+++") or l.startswith("---"):
+                l = fg.yellow(l)
+            else:
+                f = color_fn.get(l[0], str)
+                l = f(l)
+        print(l, end="")
+
+
+def check_rtd_docs_up_to_date(branch="master", colors=True) -> int:
     url = THEIR_README_URL_NO_BRANCH.format(branch)
 
     with open(OUR_README_PATH) as f:
@@ -42,23 +66,22 @@ def check_rtd_docs_up_to_date(branch="master") -> int:
         return 127
 
     print()
-    if our_readme_contents == their_readme_contents:
+    our_translated_readme_contents = translate_readme_to_rtd(OUR_README_PATH)
+    if our_translated_readme_contents == their_readme_contents:
         print(f"Docs are in sync!")
         return 0
     else:
         print(f"Docs are out of sync!")
         print()
-        our_translated_readme_contents = translate_readme_to_rtd(OUR_README_PATH)
         print("Unified diff: Local vs Remote")
         print("=============================")
         print()
-        print(
-            "".join(
-                difflib.unified_diff(
-                    our_translated_readme_contents.splitlines(keepends=True),
-                    their_readme_contents.splitlines(keepends=True),
-                )
-            )
+        print_unified_diff(
+            difflib.unified_diff(
+                our_translated_readme_contents.splitlines(keepends=True),
+                their_readme_contents.splitlines(keepends=True),
+            ),
+            colors=colors,
         )
         return 1
 
