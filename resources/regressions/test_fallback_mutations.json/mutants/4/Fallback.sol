010e776ef9fc1355ea8ab9395beb6527ff012dfc
Chandrakana Nandi <chandra.nandi90@gmail.com>
2023-09-12 16:10:55 -0700

    fall back tests

---
+++ resources/regressions/test_fallback_mutations.json/mutants/4/Fallback.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.13;
+
+contract Fallback {
+    function checkArgumentsAreReplaced(
+        uint256 num,
+        address addr,
+        bool b
+    ) public returns (uint256) {
+        if (num == 0) {
+            return 0;
+        /// ExpressionValueReplacement(`checkArgumentsAreReplaced(num - 1, addr, !b)` |==> `1`) of: `return checkArgumentsAreReplaced(num - 1, addr, !b);`
+        } else {
+            return 1;
+        }
+    }
+}
